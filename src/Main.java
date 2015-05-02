a37602c0406c75f7a439d741b2ea7c11dde72ac8
jairo_vera <vera_jairo_2016@hotmail.com>
2015-05-02 16:13:57 -0500

    changed the order of the edges to assure correct depth traversal

--- src/Main.java
+++ src/Main.java
@@ -20,19 +20,18 @@ public class Main {
 		DirectedGraph<Integer, DefaultEdge> graph = 
 	            new DefaultDirectedGraph <Integer, DefaultEdge>(DefaultEdge.class);
 
-	        graph.addVertex(7);
-	        graph.addVertex(4);
-	        graph.addVertex(9);
+		 	graph.addVertex(7);
+		 	graph.addVertex(9);
+			graph.addVertex(4);
 	        graph.addVertex(3);
 	        graph.addVertex(2);
 	        graph.addVertex(5);
 
-
-	        graph.addEdge(7, 4);
 	        graph.addEdge(7, 9);
 	        graph.addEdge(9, 3);
-	        graph.addEdge(3, 2);
 	        graph.addEdge(3, 5);
+	        graph.addEdge(3, 2);
+	        graph.addEdge(7, 4);
 
 	        GraphIterator<Integer, DefaultEdge> iterator = 
 	                new DepthFirstIterator<Integer, DefaultEdge>(graph);
