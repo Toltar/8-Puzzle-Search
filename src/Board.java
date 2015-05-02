b9e30e98fec0361881165d6d6487c20c97c4d885
Matthew <Matthew@Matthew-pc>
2015-05-02 14:40:26 -0500

    Created a toString method for Board

--- src/Board.java
+++ src/Board.java
@@ -3,31 +3,30 @@ public class Board {
 	/*index of the array represents the positions of the tiles on the board
 	 * The contents of the array will be represented from numbers 1-9 and 0 for the blank position
 	 */
-	private int[] theBoard;
+	private int[][] theBoard;
 	
-	public Board(int[] newBoard){
+	public Board(int[][] newBoard){
 		this.theBoard = newBoard;
 	}
 	
-	public int[] getBoard(){
+	public int[][] getBoard(){
 		return this.theBoard;
 	}
-	public void setBoard(int[] newBoard){
+	public void setBoard(int[][] newBoard){
 		this.theBoard = newBoard;
 	}
 	@Override
 	public String toString(){
 		String boardString="";
 		for(int x=0; x<3; x++){
+			if(x==0){
+				boardString+= "---------"+"\n";
+			}
 			for(int y=0; y<3; y++){
-				//row 1
-				if(x==0){
-					
-				}else if(x==1){
-					
-				}else if(x==2){
-					
-				}
+				boardString+= "|"+theBoard[x][y]+"|";
+			}
+			if(x==2){
+				boardString+= "\n"+"---------";
 			}
 		}
 		
