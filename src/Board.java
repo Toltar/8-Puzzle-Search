0d81d71f3ff5b861b31749dc2b25141ae0e78ea0
Matthew <Matthew@Matthew-pc>
2015-05-02 14:49:14 -0500

    Finished the Board State class and tested the toString method

--- src/Board.java
+++ src/Board.java
@@ -3,6 +3,7 @@ public class Board {
 	/*index of the array represents the positions of the tiles on the board
 	 * The contents of the array will be represented from numbers 1-9 and 0 for the blank position
 	 */
+	public final int SIZE=3;
 	private int[][] theBoard;
 	
 	public Board(int[][] newBoard){
@@ -18,15 +19,17 @@ public class Board {
 	@Override
 	public String toString(){
 		String boardString="";
-		for(int x=0; x<3; x++){
+		for(int x=0; x<SIZE; x++){
 			if(x==0){
 				boardString+= "---------"+"\n";
 			}
-			for(int y=0; y<3; y++){
+			for(int y=0; y<SIZE; y++){
 				boardString+= "|"+theBoard[x][y]+"|";
 			}
 			if(x==2){
 				boardString+= "\n"+"---------";
+			}else{
+				boardString+= "\n";
 			}
 		}
 		
--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,15 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println();
+		int[][] test = new int[3][3];
+		int n=0;
+		for(int x=0; x<3; x++){
+			for(int y=0; y<3; y++){
+				test[x][y]= n++;
+			}
+		}
+		Board state = new Board(test);
+		System.out.println(state.toString());
 	}
 
 }
