5295584718380255bcb9d6e0f4a80f7ad86fe833
Matthew <Matthew@Matthew-pc>
2015-05-02 15:17:01 -0500

    Added the stateID variable along with its methods to implement a hashTable later on(master

--- src/Board.java
+++ src/Board.java
@@ -4,18 +4,62 @@ public class Board {
 	 * The contents of the array will be represented from numbers 1-9 and 0 for the blank position
 	 */
 	public final int SIZE=3;
+	public final String NAME = "Board";
 	private int[][] theBoard;
-	
+	private double stateID;
+	/*
+	 * Constructs the newly made Board State
+	 * @param {int[][]} newBoard Input of the new board
+	 */
 	public Board(int[][] newBoard){
 		this.theBoard = newBoard;
+		setStateID();
 	}
-	
+	/*
+	 * gets the Board State of this instance
+	 */
 	public int[][] getBoard(){
 		return this.theBoard;
 	}
+	/*
+	 * Sets the Board State with a new Board State
+	 * @param {int[][]} newBoard;
+	 */
 	public void setBoard(int[][] newBoard){
 		this.theBoard = newBoard;
 	}
+	/*
+	 * gets the Board State ID for the Hash functions
+	 */
+	public double getStateID(){
+		return this.stateID;
+	}
+	/*
+	 * sets the stateID of the particularState
+	 */
+	@SuppressWarnings("null")
+	private void setStateID(){
+		double newID = 0;
+		int rowProduct=1;
+		int multiplier=1;
+		for(int x=0; x<SIZE; x++){
+			rowProduct = 1;
+			for(int y=0; y<SIZE; y++){
+				rowProduct*=(theBoard[x][y]*multiplier);
+				multiplier++;
+			}
+			System.out.println(rowProduct);
+			newID+=(double)rowProduct;
+		}
+
+		stateID = newID/9;
+		
+	}
+	/*
+	 * Modified toString method for the BoardState
+	 * (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
 	@Override
 	public String toString(){
 		String boardString="";
